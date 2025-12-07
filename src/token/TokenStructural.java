4370f218534d42fca9d6c7bd75a28b9beee0c871
Rococcoon <spacedangles@gmail.com>
2025-12-07 14:30:44 -0800

    feat: implement TokenStructural.java

--- src/token/TokenStructural.java
+++ src/token/TokenStructural.java
@@ -1,6 +1,31 @@
 package token;
 
 /**
+ * a class that represents JSON structural characters
+ * [, ], {, }, ; ','
+ * inherits from the AbstractToken class
  */
-public class TokenStructural {
+public class TokenStructural extends AbstractToken {
+
+  /**
+   * Constructor for TokenStructural
+   * 
+   * @param type    The structural TokenType
+   * @param literal The raw character strin
+   */
+  public TokenStructural(TokenType type, String literal) {
+    // Call to the AbstractToken constructor
+    super(type, literal);
+  }
+
+  /**
+   * Class specific implementation of the evaluate method signature.
+   * This token type has no real value/data so we return null
+   *
+   * @return null
+   */
+  @Override
+  public Object evaluate() {
+    return null;
+  }
 }
