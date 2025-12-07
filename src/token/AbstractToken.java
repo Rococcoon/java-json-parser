81380291d11887e009d29f87f266bf150ef8015d
Rococcoon <spacedangles@gmail.com>
2025-12-07 14:18:19 -0800

    feat: implement abstract token class and interface

--- src/token/AbstractToken.java
+++ src/token/AbstractToken.java
@@ -1,7 +1,46 @@
 package token;
 
 /**
- * Abstract class for the Tokens
+ * Abstract Class for Token objects
+ * defines what all token objects will require
  */
-public class AbstractToken {
+public abstract class AbstractToken implements Evaluable {
+
+  private final TokenType type; // The type of token, based on enum TokenType
+  private final String literal; // The litteral value of the Token
+
+  /**
+   * Constructor for the Abstract Token.
+   * 
+   * @param type    The TokenType enum value.
+   * @param literal The litteral string value.
+   */
+  public AbstractToken(TokenType type, String literal) {
+    this.type = type;
+    this.literal = literal;
+  }
+
+  // Getter methods
+
+  public TokenType getType() {
+    return type;
+  }
+
+  public String getLiteral() {
+    return literal;
+  }
+
+  /**
+   * Method uses the Evaluabble interface to return concrete Java values that
+   * can be used in programs.
+   *
+   * * @return The final Java object based on the class type
+   */
+  @Override
+  public abstract Object evaluate();
+
+  @Override
+  public String toString() {
+    return "Token [type=" + type + ", literal='" + literal + "']";
+  }
 }
--- src/token/Evaluable.java
+++ src/token/Evaluable.java
@@ -1,6 +1,15 @@
 package token;
 
 /**
+ * Interface
+ * allows us to get the value of the Token and create a usable java value
+ * each different token class will implement this bassed on their type
  */
-public class Evaluable {
+public interface Evaluable {
+  /**
+   * method signature for classes evaluate
+   * 
+   * @return The canonical Java object form of the token's value.
+   */
+  Object evaluate();
 }
